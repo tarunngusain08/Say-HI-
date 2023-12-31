28026df5a7d563ce3a1c989cc27399a0587fc585
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2023-12-31 18:35:59 +0530

    Update register.go

--- user/repo/register.go
+++ user/repo/register.go
@@ -1,1 +1,24 @@
 package repo
+
+import (
+	"Say-Hi/user/contracts"
+	"database/sql"
+)
+
+type RegisterRepo struct {
+	db *sql.DB
+}
+
+func NewRegisterRepo(db *sql.DB) *RegisterRepo {
+	return &RegisterRepo{db: db}
+}
+
+var register = `Insert into Users values ($1,$2,$3,$4)`
+
+func (r *RegisterRepo) Register(data *contracts.RegisterUser) error {
+	_, err := r.db.Exec(register, data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
