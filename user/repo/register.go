28040f56559e9e045f3280a2a351b746c1fdb372
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2023-12-31 19:46:06 +0530

    Update register.go

--- user/repo/register.go
+++ user/repo/register.go
@@ -13,10 +13,10 @@ func NewRegisterRepo(db *sql.DB) *RegisterRepo {
 	return &RegisterRepo{db: db}
 }
 
-var register = `Insert into Users values ($1,$2,$3,$4)`
+var register = `Insert into users (username, name, password, email) values ($1,$2,$3,$4)`
 
 func (r *RegisterRepo) Register(data *contracts.RegisterUser) error {
-	_, err := r.db.Exec(register, data)
+	_, err := r.db.Exec(register, data.UserName, data.Name, data.Password, data.Email)
 	if err != nil {
 		return err
 	}
