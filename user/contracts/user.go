b800f623d388d74595f3e74536c98faa710de36d
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-01-02 16:37:04 +0530

    Update user.go

--- user/contracts/user.go
+++ user/contracts/user.go
@@ -6,3 +6,14 @@ type RegisterUser struct {
 	Password string `json:"password"`
 	Email    string `json:"email"`
 }
+
+type SendEmailRequest struct {
+	Name  string `json:"name"`
+	OTP   string `json:"otp"`
+	Email string `json:"email"`
+}
+
+type VerifyEmailRequest struct {
+	OTP   string `json:"otp"`
+	Email string `json:"email"`
+}
