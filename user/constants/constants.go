b9f67514c1e0a84aa6e80821fc1c45d8266b73d8
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-01-02 16:36:53 +0530

    Create constants.go

---
+++ user/constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	SendEmailUrl      = "http://localhost:8080/api/notification/send-email"
+	VerifyEmailAction = "verify-email"
+	ExpectedOTP       = "expected-otp"
+	Unverified        = "unverified"
+	ContentType       = "application/json"
+)
