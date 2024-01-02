8484efd51f5a3d8910c00e64997283b520c2d849
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-01-02 16:38:42 +0530

    Update register.go

--- user/service/register.go
+++ user/service/register.go
@@ -1,1 +1,30 @@
 package service
+
+import (
+	"Say-Hi/user/contracts"
+	"Say-Hi/user/external"
+	"Say-Hi/user/repo"
+)
+
+type RegisterService struct {
+	repo *repo.RegisterRepo
+}
+
+func NewRegisterService(registerRepo *repo.RegisterRepo) *RegisterService {
+	return &RegisterService{repo: registerRepo}
+}
+
+func (r *RegisterService) Register(service *external.EmailService, data *contracts.RegisterUser) error {
+
+	otp, err := service.SendEmailWithExponentialBackoff(data.Email, data.Name)
+	if err != nil {
+		return err
+	}
+
+	err = r.repo.Register(data, *otp)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
