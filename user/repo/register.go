22704a4585253bf0eb70fd54f9f6f025222c626b
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-01-02 16:38:16 +0530

    Update register.go

--- user/repo/register.go
+++ user/repo/register.go
@@ -1,6 +1,7 @@
 package repo
 
 import (
+	"Say-Hi/user/constants"
 	"Say-Hi/user/contracts"
 	"database/sql"
 )
@@ -13,10 +14,10 @@ func NewRegisterRepo(db *sql.DB) *RegisterRepo {
 	return &RegisterRepo{db: db}
 }
 
-var register = `Insert into users (username, name, password, email) values ($1,$2,$3,$4)`
+var register = `Insert into users (username, name, password, email, status, otp) values ($1,$2,$3,$4,$5,$6)`
 
-func (r *RegisterRepo) Register(data *contracts.RegisterUser) error {
-	_, err := r.db.Exec(register, data.UserName, data.Name, data.Password, data.Email)
+func (r *RegisterRepo) Register(data *contracts.RegisterUser, otp string) error {
+	_, err := r.db.Exec(register, data.UserName, data.Name, data.Password, data.Email, constants.Unverified, otp)
 	if err != nil {
 		return err
 	}
