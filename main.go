1275f316789ddf422819f8c48f0ab0e95e49fa27
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-01-03 10:30:17 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,51 +2,9 @@
 package main
 
 import (
-	"Say-Hi/types"
-	userHandler "Say-Hi/user/handler"
-	userRepo "Say-Hi/user/repo"
-	"database/sql"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
-	"log"
 )
-
-var (
-	DB      *sql.DB
-	Handler *types.Handler
-)
-
-func connectDB() (*sql.DB, error) {
-	connStr := "user=tgusain dbname=tgusain sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		return nil, err
-	}
-
-	err = db.Ping()
-	if err != nil {
-		return nil, err
-	}
-
-	fmt.Println("Connected to the database")
-	return db, nil
-}
-
-func initDB() {
-	var err error
-	DB, err = connectDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func initHandlers() {
-	userRepo := userRepo.NewRegisterRepo(DB)
-	userHandler := userHandler.NewRegisterHandler(userRepo)
-	Handler = new(types.Handler)
-	Handler.User.RegisterHandler = userHandler
-}
 
 func main() {
 	initDB()
@@ -54,6 +12,12 @@ func main() {
 	defer DB.Close()
 	r := gin.New()
 	api := r.Group("/api")
-	api.POST("register", Handler.RegisterHandler.Register)
+
+	user := api.Group("/user")
+	user.POST("register", handler.User.RegisterHandler.Register)
+	user.POST("verify-email", handler.User.VerifyEmailHandler.VerifyEmail)
+
+	notification := api.Group("/notification")
+	notification.POST("send-email", handler.Notification.SendEmailHandler.SendEmail)
 	r.Run("localhost:8080")
 }
