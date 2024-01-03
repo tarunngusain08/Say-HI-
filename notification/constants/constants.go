bf4e77bde28b1bfa8806051577930102ba593e29
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-01-03 10:32:51 +0530

    Create constants.go

---
+++ notification/constants/constants.go
@@ -0,0 +1,14 @@
+package constants
+
+const (
+	From                 = "From"
+	To                   = "To"
+	Subject              = "Subject"
+	VerifyEmailSubject   = "Verify your email"
+	HostEmail            = "letzbrawl1@gmail.com"
+	ContentTextPlain     = "text/plain"
+	HostEmailAppPassword = "iwvg byzq avex ricn"
+	SMTPServerAddress    = "smtp.gmail.com"
+	SMTPServerPort       = 587
+	VerifyEmailMessage   = "Hi %v, Thank you for signing up to the MyChatApp. Use %v as the OTP to verify your email."
+)
