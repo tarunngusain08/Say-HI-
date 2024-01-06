901a6c9d0f8f22b953b6def69c6efff586a02a58
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-01-06 11:58:10 +0530

    Update secret-key-generator.go

--- auth/secret-key-generator.go
+++ auth/secret-key-generator.go
@@ -1,12 +1,13 @@
 package auth
 
 import (
+	"Say-Hi/config"
 	"crypto/rand"
 	"encoding/base64"
 )
 
 func generateRandomSecretKey() (string, error) {
-	key := make([]byte, Length)
+	key := make([]byte, config.Config.SecretKeyLength)
 	_, err := rand.Read(key)
 	if err != nil {
 		return "", err
