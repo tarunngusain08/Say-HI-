f0b53b2ba3ebb3d1605637f9535f27bd8ec680c1
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-01-08 18:13:30 +0530

    Create middleware.go

---
+++ auth/middleware.go
@@ -0,0 +1,42 @@
+package auth
+
+import (
+	"Say-Hi/config"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+var blacklist = make(map[string]bool)
+
+// When a user logs out, add their token to the blacklist
+func addToBlacklist(token string) {
+	blacklist[token] = true
+}
+
+func Middleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader(config.Authorization)
+		if tokenString == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{config.Error: config.Unauthorized})
+			c.Abort()
+			return
+		}
+
+		// Verify JWT token
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte(config.Config.SecretKey), nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{config.Error: config.Unauthorized})
+			c.Abort()
+			return
+		}
+
+		// Set user information in the context for further use
+		claims := token.Claims.(jwt.MapClaims)
+		c.Set(config.UserId, claims[config.UserId])
+		c.Next()
+	}
+}
